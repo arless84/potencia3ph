3bd2847fa20afc31e78e78cf12f4bf9dd3143a9a
arles <arles.s@hotmail.es>
2018-11-13 20:28:16 -0500

    Update web/index.php

--- web/index.php
+++ web/index.php
@@ -38,16 +38,16 @@ $app->get('/Mediciones/{v1/v2/i1/i2/i3/p1/p2}', function($v1;$v2;$i1;$i2;$i3;$p1
 
 	});
 
-$app->get('/medicion_test/{v1/v2/i1/i2/i3/p1/p2}', function($v1;$v2;$i1;$i2;$i3;$p1;$p2) use($app){
+$app->get('/medicion_test/{v1}/{v2}/{i1}/{i2}/{i3}/{p1}/{p2}', function($v1;$v2;$i1;$i2;$i3;$p1;$p2) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d83082i66m502p user=auwnpcmayyfvrf password=9c4d61ed2d9d293b80902dbfe5611d7bde523f1ec69d9853b8e349df73b2161c");
 	$registro=array(
 		"fecha"=>date('Y-m-d H:i:s'),
-		"voltaje_1"=>$v1);
-		"voltaje_2"=>$v2);
-	    "corriente_1"=>$i1);
-        "corriente_2"=>$i2);
-        "corriente_3"=>$i3);
-        "potencia_1"=>$p1);
+		"voltaje_1"=>$v1,
+		"voltaje_2"=>$v2,
+	    "corriente_1"=>$i1,
+        "corriente_2"=>$i2,
+        "corriente_3"=>$i3,
+        "potencia_1"=>$p1,
         "potencia_2"=>$p2);
 	$resultado=pg_insert ($dbconexion,"medicion_test",$registro);
 	return $resultado;
